bd4296fb6438cdaeac7f196b413b4a0a22f936ee
JFonS <joan.fonssanchez@gmail.com>
2015-06-16 22:19:57 +0200

    First commit

---
+++ main.lua
@@ -0,0 +1,19 @@
+-- Load some default values for our rectangle.
+function love.load()
+  if arg[#arg] == "-debug" then require("mobdebug").start() end
+  x, y, w, h = 20, 20, 60, 20;
+end
+ 
+-- Increase the size of the rectangle every frame.
+function love.update(dt)
+  w = w + 1;
+  h = h + 1;
+  print(h);
+end
+ 
+-- Draw a coloured rectangle.
+function love.draw()
+  love.graphics.setColor(0, 100, 100);
+  love.graphics.rectangle('fill', x, y, w, h);
+end
+
